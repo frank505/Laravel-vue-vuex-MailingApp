430dc6d478c685e5721d4489140cf22c0ab07b24
MacBook Pro <macbookpro@connector.local>
2021-02-05 01:23:01 +0100

    Add attachement and mails migrations

---
+++ Api/database/migrations/2021_02_05_000309_create_attachements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachements', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('file_name');
+            $table->integer('mail_id');    
+            $table->foreign('mail_id')->references('id')->on('mails');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachements');
+    }
+}
