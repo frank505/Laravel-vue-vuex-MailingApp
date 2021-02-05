ac915e9cf5fce8c5b7202e551f0be51a620dace9
MacBook Pro <macbookpro@connector.local>
2021-02-05 02:05:28 +0100

    added httpresponseservice

---
+++ Api/app/Services/HttpResponseService.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Http\Services;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class HttpResponseService
+{
+
+
+    public function is200Response($responseMessage)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                "success"=>true,
+                "message"=>$responseMessage
+            ], 200));
+    }
+
+    public function is200WithResponseData($responseMessage,$data)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                "success"=>true,
+                "message"=>$responseMessage,
+                "data"=>$data
+            ], 200));
+    }
+
+    public function is422Response($responseMessage)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                "success"=>false,
+                "message"=>$responseMessage
+            ], 422));
+    }
+
+
+    public function is500Response($responseMessage)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                "success"=>false,
+                "message"=>$responseMessage
+            ], 500));
+    }
+
+
+    public function is400Response($responseMessage)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                "success"=>false,
+                "message"=>$responseMessage
+            ], 400));
+    }
+
+    public function is400ResponseWithData($responseMessge,$data)
+    {
+        throw new HttpResponseException(response()->json([
+            "success"=>false,
+            "message"=>$responseMessge,
+            "data"=>$data
+        ]));
+    }
+
+
+ 
+
+}
