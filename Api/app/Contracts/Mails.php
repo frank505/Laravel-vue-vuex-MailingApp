1ab3afd5609b481db320274f872b53c309921853
MacBook Pro <macbookpro@connector.local>
2021-02-05 06:34:10 +0100

    setting reciepient models contracts

--- Api/app/Contracts/Mails.php
+++ Api/app/Contracts/Mails.php
@@ -3,5 +3,25 @@ namespace App\Contracts;
 
 interface Mails
 {
+    /**
+     * get mails based on the uuid
+     * @param $uuid
+     * @return mixed
+     */
+    public function getMails($uuid);
+
+    /**
+     * get a single mail based on the uuid
+     * @param $uuid
+     * @return mixed
+     */
+    public function getMail($uuid);
+
+    /***
+     * get mails related to a specific reciepent
+     * @param $reciepientEmail
+     * @return mixed
+     */
+    public function getMailRelatedToReciepient($reciepientEmail);
 
 }
