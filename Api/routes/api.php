0fb066bb0431dc806d4ae7f1f7edf305dbafc66f
MacBook Pro <macbookpro@connector.local>
2021-02-05 11:01:11 +0100

    added create mail endpoint

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/create-mail',[\App\Http\Controllers\MailController::class, 'createMail']);
 
 
 
